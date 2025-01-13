bb3f52ecc9c9f91999d823b7c6c0f8070ef938e3
daniloamarojf <daniloramarojf@gmail.com>
2025-01-13 12:53:53 -0300

    concluido remover voo

--- controle_passagens/alterar.py
+++ controle_passagens/alterar.py
@@ -54,66 +54,70 @@ def alterar_voo():
         
     if voo:
         print()
-        nome_cliente = cliente[0]
-        opcao_alterar = input(f'Deseja realmente alter o cliente: {nome_cliente} ? (1 - Sim/ 2 - Não): ')
+        numero_voo2 = voo[0]
+        opcao_alterar = input(f'Deseja realmente alter o cliente: {numero_voo2} ? (1 - Sim/ 2 - Não): ')
         
         if opcao_alterar == '1':
-            novo_nome = input('Nome: ')
-            novo_cpf = input('CPF: ')
-            novo_telefone = input('Telefone:')
-            nova_data_nascimento = input('Data de nascimento: ')
+            novo_numero_voo = input('Numero do Voo: ')
+            nova_origem = input('Origem: ')
+            novo_destino = input('Destino:')
+            nova_data_partida = input('Data de partida: ')
+            nova_data_chegada = input('Data de chegada: ')
+            nova_duracao = input('Duração: ')
+            
         
-            dados_cliente = (novo_nome, novo_cpf,novo_telefone, nova_data_nascimento, id_cliente)
+            dados_voo = (novo_numero_voo, nova_origem, novo_destino, nova_data_partida, nova_data_chegada, nova_duracao, id_voo)
         
-            cursor.execute('UPDATE clientes SET nome = ?, cpf = ?, telefone = ?, data_nascimento = ? WHERE id_cliente = ?',
-                (dados_cliente))
+            cursor.execute('UPDATE voo SET numero_voo = ?, origem = ?, chegada = ?, data_partida = ?, data_chegada = ?, duracao = ? WHERE id_voo = ?',
+                (dados_voo))
         
             conn.commit()
             print()
-            input('Deseja continuar alterando Clientes? Pressione enter!')
+            input('Deseja continuar alterando voos? Pressione enter!')
             conn.close()    
         elif opcao_alterar == '2':
-            input('Cliente NÃO atualizado. Pressione enter!')
+            input('Voo NÃO atualizado. Pressione enter!')
         else:
             input('Opção inválida!')
         
     else:
-        input(f'Cliente com ID {id_cliente} não encontardo. Pressione enter!')
+        input(f'Voo com ID {id_voo} não encontardo. Pressione enter!')
         
 def alterar_aeroporto(): 
         
     conn = sqlite3.connect("C:/Python/Passagens_aerea/Passagens_aerea/Banco_dados.db")
     cursor = conn.cursor()
         
-    id_cliente = input('Qual a identificação do Cliente a ser ALTERADO?: ')
+    id_aeroporto = input('Qual a identificação do aeroporto a ser ALTERADO?: ')
         
-    cursor.execute('SELECT nome FROM clientes WHERE id_cliente = ?', (id_cliente))
-    cliente = cursor.fetchone()
+    cursor.execute('SELECT nome FROM aeroporto WHERE id_aeroporto = ?', (id_aeroporto))
+    aeroporto = cursor.fetchone()
         
-    if cliente:
+    if aeroporto:
         print()
-        nome_cliente = cliente[0]
-        opcao_alterar = input(f'Deseja realmente alter o cliente: {nome_cliente} ? (1 - Sim/ 2 - Não): ')
+        nome_aeroporto2 = aeroporto[0]
+        opcao_alterar = input(f'Deseja realmente alter o cliente: {nome_aeroporto2} ? (1 - Sim/ 2 - Não): ')
         
         if opcao_alterar == '1':
-            novo_nome = input('Nome: ')
-            novo_cpf = input('CPF: ')
-            novo_telefone = input('Telefone:')
-            nova_data_nascimento = input('Data de nascimento: ')
+            novo_nome_aeroporto = input('Nome do Aeroporto: ')
+            novo_codigo_iata = input('Código IATA: ')
+            nova_cidade = input('Cidade:')
+            novo_pais = input('País : ')
         
-            dados_cliente = (novo_nome, novo_cpf,novo_telefone, nova_data_nascimento, id_cliente)
+            dados_aeroporto = (novo_nome_aeroporto, novo_codigo_iata, nova_cidade, novo_pais, id_aeroporto)
         
-            cursor.execute('UPDATE clientes SET nome = ?, cpf = ?, telefone = ?, data_nascimento = ? WHERE id_cliente = ?',
-                (dados_cliente))
+        
+            cursor.execute('UPDATE aeroporto SET nome_aeroporto = ?, codigo_iata = ?, cidade = ?, pais = ? WHERE id_aeroporto = ?',
+                (dados_aeroporto))
         
             conn.commit()
             print()
-            input('Deseja continuar alterando Clientes? Pressione enter!')
+            input('Deseja continuar alterando Aeroporto? Pressione enter!')
             conn.close()    
         elif opcao_alterar == '2':
-            input('Cliente NÃO atualizado. Pressione enter!')
+            input('Aeroporto NÃO atualizado. Pressione enter!')
         else:
             input('Opção inválida!')
         
     else:
-        input(f'Cliente com ID {id_cliente} não encontardo. Pressione enter!')
+        input(f'Aeroporto com ID {id_aeroporto} não encontardo. Pressione enter!')
